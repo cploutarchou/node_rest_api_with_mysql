afb364560f3ab6cd52aa11d819295eee0ed4b94b
Christos Ploutarchou <cploutarchou@gmail.com>
2020-04-09 12:34:43 +0300

    update routes

--- routes/index.js
+++ routes/index.js
@@ -6,26 +6,26 @@
  * Time: 01:45
  **/
 
-const tutorial = require("../controllers/Post");
+const post = require("../controllers/Post");
 const express = require("express");
 const router = express.Router();
-// Create New Tutorial
-router.post("/api/posts/create", tutorial.create);
-// // Retrieve all Tutorials
-router.get("/api/posts/all", tutorial.getAllPosts);
-// Retrieve all Published Tutorials
-router.get("/api/posts/published", tutorial.getAllPublishedPosts);
-// Retrieve all Published Tutorials by Publisher Name
-router.get("/api/posts", tutorial.getAllPostsByPublisherName);
+// Create New Post
+router.post("/api/posts/create", post.create);
+// // Retrieve all posts
+router.get("/api/posts/all", post.getAllPosts);
+// Retrieve all Published posts
+router.get("/api/posts/published", post.getAllPublishedPosts);
+// Retrieve all Published posts by Publisher Name
+router.get("/api/posts/publisher", post.getAllPostsByPublisherName);
 // Retrieve all posts by title
-router.get("/api/posts", tutorial.getPostByTitle);
-// Retrieve Tutorial by ID
-router.get("/api/posts/:id", tutorial.getPostByID);
-// // Update Tutorial by ID
-router.put("/api/post/update/:id", tutorial.updatePostByID);
-// // Delete Tutorial by ID
-router.get("/api/post/delete/:id", tutorial.deletePostByID);
-// Delete all Tutorials
-router.get("/api/posts/deleteAll", tutorial.deleteAllPosts);
+router.get("/api/posts", post.getPostByTitle);
+// Retrieve post by ID
+router.get("/api/posts/:id", post.getPostByID);
+// // Update post by ID
+router.put("/api/post/update/:id", post.updatePostByID);
+// // Delete post by ID
+router.delete("/api/post/delete/:id", post.deletePostByID);
+// Delete all posts
+router.delete("/api/posts/deleteAll", post.deleteAllPosts);
 
 module.exports = router;
